d249dad9ad1752c82ca8eb62dff083380cb6df5b
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-07-06 09:32:37 -0400

    update

--- go/flake.nix
+++ go/flake.nix
@@ -8,70 +8,70 @@
     devenv-shell.url = "github:friedenberg/eng?dir=pkgs/alfa/devenv-shell";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-stable,
-    utils,
-    devenv-go,
-    devenv-shell,
-  }:
+  outputs =
+    { self
+    , nixpkgs
+    , nixpkgs-stable
+    , utils
+    , devenv-go
+    , devenv-shell
+    ,
+    }:
     (utils.lib.eachDefaultSystem
       (system:
-        let
+      let
 
-          pkgs = import nixpkgs {
-            inherit system;
+        pkgs = import nixpkgs {
+          inherit system;
 
-            overlays = [
-              devenv-go.overlays.default
-            ];
+          overlays = [
+            devenv-go.overlays.default
+          ];
+        };
+
+        dodder = pkgs.buildGoApplication {
+          pname = "dodder";
+          version = "0.0.1";
+          src = ./.;
+          subPackages = [
+            "cmd/der"
+            # "cmd/dodder"
+          ];
+          modules = ./gomod2nix.toml;
+        };
+
+      in
+      {
+
+        packages.dodder = dodder;
+        packages.default = dodder;
+
+        docker = pkgs.dockerTools.buildImage {
+          name = "dodder";
+          tag = "latest";
+          copyToRoot = [ dodder ];
+          config = {
+            Cmd = [ "${dodder}/bin/dodder" ];
+            Env = [ ];
+            ExposedPorts = { "9000/tcp" = { }; };
           };
+        };
 
-          dodder = pkgs.buildGoApplication {
-            pname = "dodder";
-            version = "0.0.1";
-            src = ./.;
-            subPackages = [
-              "cmd/der"
-              "cmd/dodder"
-              "cmd/zit"
-            ];
-            modules = ./gomod2nix.toml;
-          };
+        devShells.default = pkgs.mkShell {
+          # inherit (gomod2nix.packages.${system}) mkGoEnv gomod2nix;
 
-        in {
+          packages = (with pkgs; [
+            bats
+            fish
+            gnumake
+            just
+          ]);
 
-          packages.dodder = dodder;
-          packages.default = dodder;
-
-          docker = pkgs.dockerTools.buildImage {
-            name = "dodder";
-            tag = "latest";
-            copyToRoot = [dodder];
-            config = {
-              Cmd = ["${dodder}/bin/dodder"];
-              Env = [];
-              ExposedPorts = {"9000/tcp" = {};};
-            };
-          };
-
-          devShells.default = pkgs.mkShell {
-            # inherit (gomod2nix.packages.${system}) mkGoEnv gomod2nix;
-
-            packages = (with pkgs; [
-              govulncheck
-              bats
-              fish
-              gnumake
-              just
-            ]);
-
-            inputsFrom = [
-              devenv-go.devShells.${system}.default
-              devenv-shell.devShells.${system}.default
-            ];
-          };
-        })
+          inputsFrom = [
+            devenv-go.devShells.${system}.default
+            devenv-shell.devShells.${system}.default
+          ];
+        };
+      })
     );
 }
