98759641c11c91f22ebe8b4a4aa4aa966a591f32
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-07-13 20:41:19 -0400

    update

---
+++ go/src/quebec/commands/blob_store_cat.go
@@ -0,0 +1,169 @@
+package commands
+
+import (
+	"flag"
+	"io"
+	"os/exec"
+
+	"code.linenisgreat.com/dodder/go/src/alfa/errors"
+	"code.linenisgreat.com/dodder/go/src/alfa/interfaces"
+	"code.linenisgreat.com/dodder/go/src/bravo/quiter"
+	"code.linenisgreat.com/dodder/go/src/bravo/ui"
+	"code.linenisgreat.com/dodder/go/src/charlie/delim_io"
+	"code.linenisgreat.com/dodder/go/src/delta/script_value"
+	"code.linenisgreat.com/dodder/go/src/delta/sha"
+	"code.linenisgreat.com/dodder/go/src/golf/command"
+	"code.linenisgreat.com/dodder/go/src/golf/env_ui"
+	"code.linenisgreat.com/dodder/go/src/november/local_working_copy"
+	"code.linenisgreat.com/dodder/go/src/papa/command_components"
+)
+
+func init() {
+	command.Register("cat-blob", &BlobStoreCat{})
+	command.Register("blob_store-cat", &BlobStoreCat{})
+}
+
+type BlobStoreCat struct {
+	command_components.BlobStoreLocal
+
+	Utility   script_value.Utility
+	PrefixSha bool
+}
+
+func (cmd *BlobStoreCat) SetFlagSet(f *flag.FlagSet) {
+	f.Var(&cmd.Utility, "utility", "")
+	f.BoolVar(&cmd.PrefixSha, "prefix-sha", false, "")
+}
+
+type shaWithReadCloser struct {
+	Sha        *sha.Sha
+	ReadCloser io.ReadCloser
+}
+
+func (c BlobStoreCat) makeBlobWriter(
+	blobStore command_components.BlobStoreWithEnv,
+) interfaces.FuncIter[shaWithReadCloser] {
+	if c.Utility.IsEmpty() {
+		return quiter.MakeSyncSerializer(
+			func(rc shaWithReadCloser) (err error) {
+				if err = c.copy(blobStore, rc); err != nil {
+					err = errors.Wrap(err)
+					return
+				}
+
+				return
+			},
+		)
+	} else {
+		return quiter.MakeSyncSerializer(
+			func(rc shaWithReadCloser) (err error) {
+				defer errors.DeferredCloser(&err, rc.ReadCloser)
+
+				cmd := exec.Command(c.Utility.Head(), c.Utility.Tail()...)
+				cmd.Stdin = rc.ReadCloser
+
+				var out io.ReadCloser
+
+				if out, err = cmd.StdoutPipe(); err != nil {
+					err = errors.Wrap(err)
+					return
+				}
+
+				if err = cmd.Start(); err != nil {
+					err = errors.Wrap(err)
+					return
+				}
+
+				if err = c.copy(
+					blobStore,
+					shaWithReadCloser{
+						Sha:        rc.Sha,
+						ReadCloser: out,
+					},
+				); err != nil {
+					err = errors.Wrap(err)
+					return
+				}
+
+				if err = cmd.Wait(); err != nil {
+					err = errors.Wrap(err)
+					return
+				}
+
+				return
+			},
+		)
+	}
+}
+
+func (cmd BlobStoreCat) Run(
+	dep command.Request,
+) {
+	blobStore := cmd.MakeBlobStoreLocal(
+		dep,
+		dep.Config,
+		env_ui.Options{},
+		local_working_copy.OptionsEmpty,
+	)
+
+	blobWriter := cmd.makeBlobWriter(blobStore)
+
+	for _, v := range dep.PopArgs() {
+		var sh sha.Sha
+
+		if err := sh.Set(v); err != nil {
+			blobStore.CancelWithError(err)
+		}
+
+		if err := cmd.blob(blobStore, &sh, blobWriter); err != nil {
+			ui.Err().Print(err)
+		}
+	}
+}
+
+func (c BlobStoreCat) copy(
+	blobStore command_components.BlobStoreWithEnv,
+	rc shaWithReadCloser,
+) (err error) {
+	defer errors.DeferredCloser(&err, rc.ReadCloser)
+
+	if c.PrefixSha {
+		if _, err = delim_io.CopyWithPrefixOnDelim(
+			'\n',
+			rc.Sha.GetShaLike().GetShaString(),
+			blobStore.GetUI(),
+			rc.ReadCloser,
+			true,
+		); err != nil {
+			err = errors.Wrap(err)
+			return
+		}
+	} else {
+		if _, err = io.Copy(blobStore.GetUIFile(), rc.ReadCloser); err != nil {
+			err = errors.Wrap(err)
+			return
+		}
+	}
+
+	return
+}
+
+func (c BlobStoreCat) blob(
+	blobStore command_components.BlobStoreWithEnv,
+	sh *sha.Sha,
+	blobWriter interfaces.FuncIter[shaWithReadCloser],
+) (err error) {
+	var r sha.ReadCloser
+
+	if r, err = blobStore.BlobReader(sh); err != nil {
+		err = errors.Wrap(err)
+		return
+	}
+
+	if err = blobWriter(shaWithReadCloser{Sha: sh, ReadCloser: r}); err != nil {
+		err = errors.Wrap(err)
+		return
+	}
+
+	return
+}
---
+++ go/src/quebec/commands/blob_store_write.go
@@ -0,0 +1,151 @@
+package commands
+
+import (
+	"flag"
+	"io"
+	"sync/atomic"
+
+	"code.linenisgreat.com/dodder/go/src/alfa/errors"
+	"code.linenisgreat.com/dodder/go/src/alfa/interfaces"
+	"code.linenisgreat.com/dodder/go/src/bravo/ui"
+	"code.linenisgreat.com/dodder/go/src/delta/script_value"
+	"code.linenisgreat.com/dodder/go/src/delta/sha"
+	"code.linenisgreat.com/dodder/go/src/echo/env_dir"
+	"code.linenisgreat.com/dodder/go/src/golf/command"
+	"code.linenisgreat.com/dodder/go/src/golf/env_ui"
+	"code.linenisgreat.com/dodder/go/src/november/local_working_copy"
+	"code.linenisgreat.com/dodder/go/src/papa/command_components"
+)
+
+func init() {
+	command.Register("write-blob", &BlobStoreWrite{})
+	command.Register("blob_store-write", &BlobStoreWrite{})
+}
+
+type BlobStoreWrite struct {
+	command_components.BlobStoreLocal
+
+	Check         bool
+	UtilityBefore script_value.Utility
+	UtilityAfter  script_value.Utility
+}
+
+func (cmd *BlobStoreWrite) SetFlagSet(flagSet *flag.FlagSet) {
+	flagSet.BoolVar(
+		&cmd.Check,
+		"check",
+		false,
+		"only check if the object already exists",
+	)
+
+	flagSet.Var(&cmd.UtilityBefore, "utility-before", "")
+	flagSet.Var(&cmd.UtilityAfter, "utility-after", "")
+}
+
+type answer struct {
+	error
+	interfaces.Sha
+	Path string
+}
+
+func (cmd BlobStoreWrite) Run(
+	dep command.Request,
+) {
+	blobStore := cmd.MakeBlobStoreLocal(
+		dep,
+		dep.Config,
+		env_ui.Options{},
+		local_working_copy.OptionsEmpty,
+	)
+
+	var failCount atomic.Uint32
+
+	sawStdin := false
+
+	for _, p := range dep.PopArgs() {
+		switch {
+		case sawStdin:
+			ui.Err().Print("'-' passed in more than once. Ignoring")
+			continue
+
+		case p == "-":
+			sawStdin = true
+		}
+
+		a := answer{Path: p}
+
+		a.Sha, a.error = cmd.doOne(blobStore, p)
+
+		if a.error != nil {
+			blobStore.GetErr().Printf("%s: (error: %q)", a.Path, a.error)
+			failCount.Add(1)
+			continue
+		}
+
+		hasBlob := blobStore.HasBlob(a.Sha)
+
+		if hasBlob {
+			if cmd.Check {
+				blobStore.GetUI().Printf(
+					"%s %s (already checked in)",
+					a.GetShaLike(),
+					a.Path,
+				)
+			} else {
+				blobStore.GetUI().Printf("%s %s (checked in)", a.GetShaLike(), a.Path)
+			}
+		} else {
+			ui.Err().Printf("%s %s (untracked)", a.GetShaLike(), a.Path)
+
+			if cmd.Check {
+				failCount.Add(1)
+			}
+		}
+	}
+
+	fc := failCount.Load()
+
+	if fc > 0 {
+		blobStore.CancelWithBadRequestf("untracked objects: %d", fc)
+		return
+	}
+}
+
+func (cmd BlobStoreWrite) doOne(
+	blobStore command_components.BlobStoreWithEnv,
+	path string,
+) (sh interfaces.Sha, err error) {
+	var readCloser io.ReadCloser
+
+	if readCloser, err = env_dir.NewFileReader(
+		env_dir.DefaultConfig,
+		path,
+	); err != nil {
+		err = errors.Wrap(err)
+		return
+	}
+
+	defer errors.DeferredCloser(&err, readCloser)
+
+	var writeCloser sha.WriteCloser
+
+	if cmd.Check {
+		writeCloser = sha.MakeWriter(nil)
+	} else {
+		if writeCloser, err = blobStore.BlobWriter(); err != nil {
+			err = errors.Wrap(err)
+			return
+		}
+	}
+
+	defer errors.DeferredCloser(&err, writeCloser)
+
+	if _, err = io.Copy(writeCloser, readCloser); err != nil {
+		err = errors.Wrap(err)
+		return
+	}
+
+	sh = writeCloser.GetShaLike()
+
+	return
+}
