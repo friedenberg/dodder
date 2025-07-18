675eb35411d3bc322a7ae4941bb72ba12c1ca87d
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-07-18 11:42:33 -0400

    update

--- go/flake.nix
+++ go/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "flake:nixpkgs";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2505.806427.tar.gz";
     nixpkgs-stable.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2411.717296.tar.gz";
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102.tar.gz";
 
