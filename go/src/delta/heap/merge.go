dc7153ceeeb39b8e8aef3dcd194b4b8dfc62394d
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-09-27 12:01:09 -0400

    update (pre codegen and test)

--- go/src/delta/heap/merge.go
+++ go/src/delta/heap/merge.go
@@ -5,28 +5,28 @@ import (
 	"code.linenisgreat.com/dodder/go/src/alfa/interfaces"
 )
 
-func MergeHeap[
-	ELEMENT Element,
-	ELEMENT_PTR ElementPtr[ELEMENT],
-](
-	heap *Heap[ELEMENT, ELEMENT_PTR],
-	read func() (ELEMENT_PTR, error),
-	write interfaces.FuncIter[ELEMENT_PTR],
-) (err error) {
-	if err = MergeStreamPreferringHeap(
-		heap,
-		read,
-		write,
-		heap.private.equaler,
-		heap.private.Lessor,
-		heap.private.Resetter,
-	); err != nil {
-		err = errors.Wrap(err)
-		return err
-	}
+// func MergeHeap[
+// 	ELEMENT Element,
+// 	ELEMENT_PTR ElementPtr[ELEMENT],
+// ](
+// 	heap *Heap[ELEMENT, ELEMENT_PTR],
+// 	read func() (ELEMENT_PTR, error),
+// 	write interfaces.FuncIter[ELEMENT_PTR],
+// ) (err error) {
+// 	if err = MergeStreamPreferringHeap(
+// 		heap,
+// 		read,
+// 		write,
+// 		heap.private.equaler,
+// 		heap.private.Lessor,
+// 		heap.private.Resetter,
+// 	); err != nil {
+// 		err = errors.Wrap(err)
+// 		return err
+// 	}
 
-	return err
-}
+// 	return err
+// }
 
 func MergeHeapAndRestore[
 	ELEMENT Element,
@@ -59,127 +59,126 @@ type stream[
 	Pop() (ELEMENT_PTR, bool)
 }
 
-func MergeStreamPreferringHeap[
-	ELEMENT Element,
-	ELEMENT_PTR ElementPtr[ELEMENT],
-](
-	heap stream[ELEMENT, ELEMENT_PTR],
-	read func() (ELEMENT_PTR, error),
-	write interfaces.FuncIter[ELEMENT_PTR],
-	equaler interfaces.Equaler[ELEMENT_PTR],
-	lessor interfaces.Lessor3[ELEMENT_PTR],
-	resetter interfaces.Resetter2[ELEMENT, ELEMENT_PTR],
-) (err error) {
-	oldWrite := write
+// func MergeStreamPreferringHeap[
+// 	ELEMENT Element,
+// 	ELEMENT_PTR ElementPtr[ELEMENT],
+// ](
+// 	heap stream[ELEMENT, ELEMENT_PTR],
+// 	read func() (ELEMENT_PTR, error),
+// 	write interfaces.FuncIter[ELEMENT_PTR],
+// 	equaler interfaces.Equaler[ELEMENT_PTR],
+// 	lessor interfaces.Lessor3[ELEMENT_PTR],
+// 	resetter interfaces.Resetter2[ELEMENT, ELEMENT_PTR],
+// ) (err error) {
+// 	oldWrite := write
 
-	var lastElement ELEMENT_PTR
+// 	var lastElement ELEMENT_PTR
 
-	write = func(e ELEMENT_PTR) (err error) {
-		if lastElement == nil {
-			var t ELEMENT
-			lastElement = &t
-		} else if equaler.Equals(e, lastElement) {
-			return err
-		} else if lessor.Less(e, lastElement) {
-			err = errors.ErrorWithStackf(
-				"last is greater than current! last:\n%v\ncurrent: %v",
-				lastElement,
-				e,
-			)
+// 	write = func(e ELEMENT_PTR) (err error) {
+// 		if lastElement == nil {
+// 			var t ELEMENT
+// 			lastElement = &t
+// 		} else if equaler.Equals(e, lastElement) {
+// 			return err
+// 		} else if lessor.Less(e, lastElement) {
+// 			err = errors.ErrorWithStackf(
+// 				"last is greater than current! last:\n%v\ncurrent: %v",
+// 				lastElement,
+// 				e,
+// 			)
 
-			return err
-		}
+// 			return err
+// 		}
 
-		resetter.ResetWith(lastElement, e)
+// 		resetter.ResetWith(lastElement, e)
 
-		return oldWrite(e)
-	}
+// 		return oldWrite(e)
+// 	}
 
-	// write all the mingled elements
+// 	// write all the mingled elements
 
-	for {
-		var element ELEMENT_PTR
+// 	for {
+// 		var element ELEMENT_PTR
 
-		if element, err = read(); err != nil {
-			if errors.IsStopIteration(err) {
-				err = nil
-				break
-			} else {
-				err = errors.Wrap(err)
-				return err
-			}
-		}
+// 		if element, err = read(); err != nil {
+// 			if errors.IsStopIteration(err) {
+// 				err = nil
+// 				break
+// 			} else {
+// 				err = errors.Wrap(err)
+// 				return err
+// 			}
+// 		}
 
-	READ_FROM_HEAP:
-		for {
-			peekedElement, hasElement := heap.Peek()
+// 	READ_FROM_HEAP:
+// 		for {
+// 			peekedElement, hasElement := heap.Peek()
 
-			switch {
-			case !hasElement:
-				break READ_FROM_HEAP
+// 			switch {
+// 			case !hasElement:
+// 				break READ_FROM_HEAP
 
-			case equaler.Equals(peekedElement, element):
-				element = peekedElement
-				heap.Pop()
-				continue READ_FROM_HEAP
+// 			case equaler.Equals(peekedElement, element):
+// 				element = peekedElement
+// 				heap.Pop()
+// 				continue READ_FROM_HEAP
 
-			case lessor.Less(element, peekedElement):
-				break READ_FROM_HEAP
+// 			case lessor.Less(element, peekedElement):
+// 				break READ_FROM_HEAP
 
-			default:
-			}
+// 			default:
+// 			}
 
-			if err = write(peekedElement); err != nil {
-				if errors.IsStopIteration(err) {
-					err = nil
-				} else {
-					err = errors.Wrap(err)
-				}
+// 			if err = write(peekedElement); err != nil {
+// 				if errors.IsStopIteration(err) {
+// 					err = nil
+// 				} else {
+// 					err = errors.Wrap(err)
+// 				}
 
-				return err
-			}
-		}
+// 				return err
+// 			}
+// 		}
 
-		if err = write(element); err != nil {
-			if errors.IsStopIteration(err) {
-				err = nil
-			} else {
-				err = errors.Wrap(err)
-			}
+// 		if err = write(element); err != nil {
+// 			if errors.IsStopIteration(err) {
+// 				err = nil
+// 			} else {
+// 				err = errors.Wrap(err)
+// 			}
 
-			return err
-		}
-	}
+// 			return err
+// 		}
+// 	}
 
-	// write all the new elements
+// 	// write all the new elements
 
-	for {
-		popped, ok := heap.Pop()
+// 	for {
+// 		popped, ok := heap.Pop()
 
-		if !ok {
-			break
-		}
+// 		if !ok {
+// 			break
+// 		}
 
-		if err = write(popped); err != nil {
-			if errors.IsStopIteration(err) {
-				err = nil
-			} else {
-				err = errors.Wrap(err)
-			}
+// 		if err = write(popped); err != nil {
+// 			if errors.IsStopIteration(err) {
+// 				err = nil
+// 			} else {
+// 				err = errors.Wrap(err)
+// 			}
 
-			return err
-		}
-	}
+// 			return err
+// 		}
+// 	}
 
-	return err
-}
+// 	return err
+// }
 
 type streamWithRestore[
 	ELEMENT Element,
 	ELEMENT_PTR ElementPtr[ELEMENT],
 ] interface {
-	Peek() (ELEMENT_PTR, bool)
-	Pop() (ELEMENT_PTR, bool)
+	stream[ELEMENT, ELEMENT_PTR]
 
 	restore()
 	popAndSave() (ELEMENT_PTR, bool)
@@ -189,75 +188,75 @@ func MergeStreamPreferringHeapAndRestore[
 	ELEMENT Element,
 	ELEMENT_PTR ElementPtr[ELEMENT],
 ](
-	heap streamWithRestore[ELEMENT, ELEMENT_PTR],
-	read func() (ELEMENT_PTR, error),
+	heapAdditions streamWithRestore[ELEMENT, ELEMENT_PTR],
+	readHistory func() (ELEMENT_PTR, error),
 	write interfaces.FuncIter[ELEMENT_PTR],
 	equaler interfaces.Equaler[ELEMENT_PTR],
 	lessor interfaces.Lessor3[ELEMENT_PTR],
 	resetter interfaces.Resetter2[ELEMENT, ELEMENT_PTR],
 ) (err error) {
 	defer func() {
-		heap.restore()
+		heapAdditions.restore()
 	}()
 
 	oldWrite := write
 
 	var lastElement ELEMENT_PTR
 
-	write = func(e ELEMENT_PTR) (err error) {
+	write = func(element ELEMENT_PTR) (err error) {
 		if lastElement == nil {
-			var t ELEMENT
-			lastElement = &t
-		} else if equaler.Equals(e, lastElement) {
+			var elementValue ELEMENT
+			lastElement = &elementValue
+		} else if equaler.Equals(element, lastElement) {
 			return err
-		} else if lessor.Less(e, lastElement) {
+		} else if lessor.Less(element, lastElement) {
 			err = errors.ErrorWithStackf(
 				"last is greater than current! last:\n%v\ncurrent: %v",
 				lastElement,
-				e,
+				element,
 			)
 
 			return err
 		}
 
-		resetter.ResetWith(lastElement, e)
+		resetter.ResetWith(lastElement, element)
 
-		return oldWrite(e)
+		return oldWrite(element)
 	}
 
 	for {
 		var element ELEMENT_PTR
 
-		if element, err = read(); err != nil {
+		if element, err = readHistory(); err != nil {
 			if errors.IsStopIteration(err) {
 				err = nil
-				break
+				goto APPEND_ADDITIONS
 			} else {
 				err = errors.Wrap(err)
 				return err
 			}
 		}
 
-	LOOP:
+	INTERLACE:
 		for {
-			peekedElement, hasElement := heap.Peek()
+			peekedElement, hasElement := heapAdditions.Peek()
 
 			switch {
 			case !hasElement:
-				break LOOP
+				break INTERLACE
 
 			case equaler.Equals(peekedElement, element):
 				element = peekedElement
-				heap.Pop()
-				continue LOOP
+				heapAdditions.Pop()
+				continue INTERLACE
 
 			case lessor.Less(element, peekedElement):
-				break LOOP
+				break INTERLACE
 
 			default:
 			}
 
-			poppedElement, _ := heap.popAndSave()
+			poppedElement, _ := heapAdditions.popAndSave()
 
 			if !equaler.Equals(peekedElement, poppedElement) {
 				err = errors.ErrorWithStackf(
@@ -270,7 +269,7 @@ func MergeStreamPreferringHeapAndRestore[
 			}
 
 			if poppedElement == nil {
-				break
+				break INTERLACE
 			}
 
 			if err = write(poppedElement); err != nil {
@@ -295,8 +294,9 @@ func MergeStreamPreferringHeapAndRestore[
 		}
 	}
 
+APPEND_ADDITIONS:
 	for {
-		popped, ok := heap.popAndSave()
+		popped, ok := heapAdditions.popAndSave()
 
 		if !ok {
 			break
