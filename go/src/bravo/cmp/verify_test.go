df087cc6638b61fd31af5d463e508e6aacb8412d
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-09-29 10:19:30 -0400

    update (pre codegen and test)

--- go/src/bravo/cmp/verify_test.go
+++ go/src/bravo/cmp/verify_test.go
@@ -30,3 +30,28 @@ func TestLessorVerify(t1 *testing.T) {
 		t1.Errorf("expected 2 to not be less than 1")
 	}
 }
+
+func TestLessorEqualer(t1 *testing.T) {
+	cmp := func(left, right int) Result {
+		if left < right {
+			return Less
+		} else if left == right {
+			return Equal
+		} else {
+			return Greater
+		}
+	}
+
+	verifier := EqualerVerify[int]([]interfaces.Equaler[int]{
+		Equaler[int](cmp),
+		Equaler[int](cmp),
+	})
+
+	if !verifier.Equals(1, 1) {
+		t1.Errorf("expected 1 to equal 1")
+	}
+
+	if verifier.Equals(2, 1) {
+		t1.Errorf("expected 2 to not equal 1")
+	}
+}
