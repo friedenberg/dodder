6cfb8747c3f32d63ed599d339489b3ce028e41a1
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-10-30 09:00:41 -0400

    update

--- go/src/echo/repo_blobs/main.go
+++ go/src/echo/repo_blobs/main.go
@@ -26,10 +26,10 @@ type BlobMutable interface {
 type TypedBlob = triple_hyphen_io.TypedBlob[*Blob]
 
 var typedCoders = map[string]interfaces.CoderBufferedReadWriter[*TypedBlob]{
-	ids.TypeTomlRepoLocalOverridePath:   coderToml[TomlLocalOverridePathV0]{},
-	ids.TypeTomlRepoDotenvXdgV0: coderToml[TomlXDGV0]{},
-	ids.TypeTomlRepoUri:         coderToml[TomlUriV0]{},
-	"":                          coderToml[TomlUriV0]{},
+	ids.TypeTomlRepoLocalOverridePath: coderToml[TomlLocalOverridePathV0]{},
+	ids.TypeTomlRepoDotenvXdgV0:       coderToml[TomlXDGV0]{},
+	ids.TypeTomlRepoUri:               coderToml[TomlUriV0]{},
+	"":                                coderToml[TomlUriV0]{},
 }
 
 var Coder = interfaces.CoderBufferedReadWriter[*TypedBlob](
