90d408c21d3e83b6f26066a3671f4aafcae85b76
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-12-08 07:45:00 -0500

    update (pre codegen and test)

--- go/src/bravo/doddish/token.go
+++ go/src/bravo/doddish/token.go
@@ -1,13 +1,22 @@
 package doddish
 
 import (
+	"encoding"
 	"slices"
+
+	"code.linenisgreat.com/dodder/go/src/alfa/errors"
 )
 
 type Token struct {
 	Type     TokenType
 	Contents []byte
 }
+
+var (
+	_ encoding.BinaryMarshaler   = Token{}
+	_ encoding.BinaryAppender    = Token{}
+	_ encoding.BinaryUnmarshaler = &Token{}
+)
 
 func (token Token) String() string {
 	return string(token.Contents)
@@ -19,25 +28,25 @@ func (token Token) Clone() (dst Token) {
 	return dst
 }
 
-// func (token Token) MarshalBinary(bites []byte) ([]byte, error) {
-// 	return token.AppendBinary(bites)
-// }
+func (token Token) MarshalBinary() ([]byte, error) {
+	return token.AppendBinary(nil)
+}
 
-// func (token Token) AppendBinary(bites []byte) ([]byte, error) {
-// 	bites = slices.Grow(bites, len(token.Contents)+1)
-// 	bites = append(bites, byte(token.Type))
-// 	bites = append(bites, token.Contents...)
-// 	return bites, nil
-// }
+func (token Token) AppendBinary(bites []byte) ([]byte, error) {
+	bites = slices.Grow(bites, len(token.Contents)+1)
+	bites = append(bites, byte(token.Type))
+	bites = append(bites, token.Contents...)
+	return bites, nil
+}
 
-// func (token *Token) UnmarshalBinary(bites []byte) (err error) {
-// 	if len(bites) < 2 {
-// 		err = errors.Errorf("expected at least two bytes but got %x", bites)
-// 		return
-// 	}
+func (token *Token) UnmarshalBinary(bites []byte) (err error) {
+	if len(bites) < 2 {
+		err = errors.Errorf("expected at least two bytes but got %x", bites)
+		return
+	}
 
-// 	token.Type = TokenType(bites[0])
-// 	token.Contents = bites[1:]
+	token.Type = TokenType(bites[0])
+	token.Contents = bites[1:]
 
-// 	return
-// }
+	return
+}
